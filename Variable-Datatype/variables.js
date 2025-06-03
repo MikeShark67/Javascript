b3af6b81a5e0f9adaeb5988e498039225652b8b5
MikeShark67 <oumam504@gmail.com>
2025-06-03 15:03:07 +0300

    initialization and declaration

--- Variable-Datatype/variables.js
+++ Variable-Datatype/variables.js
@@ -1,9 +1,13 @@
 #!/usr/bin/env node
+//Var declaration
 let age;
-const name ;
+const name ="Michael";
 let number;
 let area;
-
+//var initialization
+age =(20)
+number =(67)
+area =(58)
 
 //Calling my Variable
 console.log (age)
